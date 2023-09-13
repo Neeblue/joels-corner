c2f93558f4f786fa2cecd1d0410ae9b68a2343df
Joel Miles <joelmiles@hotmail.co.uk>
2023-09-13 16:42:21 -0700

    Makes use of updated GitHubProjectList

--- src/App.js
+++ src/App.js
@@ -2,29 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Card from "./Card";
 import Contact from "./components/Contacts";
-import Projects from "./components/Projects";
+import GitHubProjectList from "./components/GitHubProjectList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="https://reactjs.org" target="_blank"  rel="noopener noreferrer">Learn React</a>
         <p />
         <Card />
         <Card />
         <p />
-        <Projects />
+        <GitHubProjectList />
       </header>
     </div>
   );
