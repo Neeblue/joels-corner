ab1df04e97658bcc997900bbde3bb234ddcdd118
Joel Miles <98841710+Neeblue@users.noreply.github.com>
2023-09-17 01:01:59 +0000

    Added max image size

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,27 @@
 import react from 'react';
 
-const ProjectCard = ({ title, description, imageUrl, githubLink }) => {
-    return (
-      <div className="project-card">
-        <a href={githubLink} target="_blank" rel="noopener noreferrer">
-          <h2>{title}</h2>
-        </a>
-        <p>{description}</p>
-        <img src={imageUrl} alt={title} width={300} />
-      </div>
-    );
-  };
+// const ProjectCard = ({ title, description, imageUrl, githubLink }) => {
+//     return (
+//       <div className="project-card">
+//         <a href={githubLink} target="_blank" rel="noopener noreferrer">
+//           <h2>{title}</h2>
+//         </a>
+//         <p>{description}</p>
+//         <img src={imageUrl} alt={title} width={300} />
+//       </div>
+//     );
+//   };
+
+const ProjectCard = ({ props }) => {
+  return (
+          <div className="project-card">
+            <a href={props.githubLink} target="_blank" rel="noopener noreferrer">
+              <h2>{props.title}</h2>
+            </a>
+            <p>{props.description}</p>
+            <img src={props.imageUrl} alt={props.title} width={300} />
+          </div>
+        );
+      };
 
  export default ProjectCard;
