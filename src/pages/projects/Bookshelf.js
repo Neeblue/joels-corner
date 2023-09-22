e1db1546ca9270e96a2b1000ab5e9665293ac5eb
Joel Miles <98841710+Neeblue@users.noreply.github.com>
2023-09-22 17:56:27 +0000

    Initial code for the Bookshelf details page

--- src/pages/projects/Bookshelf.js
+++ src/pages/projects/Bookshelf.js
@@ -1,3 +1,14 @@
+import jsonData from '../../projects.json';
+
 export default function Bookshelf() {
-    return ( <h1>Bookshelf working</h1> );
+    //const project = JsonData.projects[1];
+    // const project = JsonData.projects.find(project => project.title === "Bookshelf");
+    const project = jsonData.find(project => project.title === "Bookshelf Mobile App") ?? jsonData.projects[1];
+
+    return (
+        <div>
+            <h1>{project.title}</h1>
+            <div>{project.detailsDescription}</div>
+        </div>
+    );
 }
