795004b8df31aae2aeafa7d3c04728526599290b
Joel Miles <98841710+Neeblue@users.noreply.github.com>
2023-09-22 18:11:17 +0000

    WIP adding details and images

--- src/pages/projects/Bookshelf.js
+++ src/pages/projects/Bookshelf.js
@@ -1,14 +1,17 @@
 import jsonData from '../../projects.json';
 
 export default function Bookshelf() {
-    //const project = JsonData.projects[1];
-    // const project = JsonData.projects.find(project => project.title === "Bookshelf");
     const project = jsonData.find(project => project.title === "Bookshelf Mobile App") ?? jsonData.projects[1];
 
     return (
         <div>
             <h1>{project.title}</h1>
             <div>{project.detailsDescription}</div>
+            <img className='detailsImage' src={process.env.PUBLIC_URL + project.detailsImage1} alt="image missing!" />
+            <img className='detailsImage' src={process.env.PUBLIC_URL + project.detailsImage2} alt="image missing!" />
+
+            <img src={project.detailsImage1} />
+            <img src={project.detailsImage2} />
         </div>
     );
 }
