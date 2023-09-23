e1cb8e6696ae34d4a78e20564782c9eb9b7ef5c4
Joel Miles <98841710+Neeblue@users.noreply.github.com>
2023-09-23 05:30:40 +0000

    Bookshelf display completed

--- src/pages/projects/Bookshelf.js
+++ src/pages/projects/Bookshelf.js
@@ -6,29 +6,42 @@ export default function Bookshelf() {
     return (
         <div className="Bookshelf">
             <h1 className="title">{project.title}</h1>
-            <div className="firstSection">
+
+            {project.detailsDescription.map((paragraph, index) => (
+                <div key={index}>
+                    {index % 2 === 0 ? (
+                        <div className='section'>
+                            <img className='detailsImage' src={process.env.PUBLIC_URL + project.detailsImage[index]} alt= "" />
+                            <div className="detailsParagraph" dangerouslySetInnerHTML={{__html: paragraph}}></div>
+                        </div>
+                        
+                    ) : (
+                        <div className='section'>
+                            <div className="detailsParagraph" dangerouslySetInnerHTML={{__html: paragraph}}></div>
+                            <img className='detailsImage' src={process.env.PUBLIC_URL + project.detailsImage[index]} alt="" />
+                        </div>
+                    )}
+                    
+                </div>
+            ))}
+
+
+
+
+
+
+            {/* <div className="firstSection">
                 <div className="firstParagraph">
-                    A .Net MAUI variant of the Librarian project. This is a follow-along/modification of James Montemagno's Monkeys project.
-<br></br><br></br>
-This app has been tested on Windows and Android. It has a different display of the book details depending on the platform.  Books can be added with a search function. This app uses the Google API and the HtmlAgility package to scrape a book image and rating from Goodreads. 
-<br></br><br></br>
-Tapping on one of the books brings up a larger image with additional information (such as a description, rating, publisher info etc.). Books can be deleted by swiping the book to the left.
-<br></br><br></br>
-Additional features that are implemented:
-<ui>
-    <li>MVVM with dependency injection</li>
-    <li>uses .Net Community Toolkit 8.1</li>
-    <li>Files are stored in the cloud using Firebase (Firesharp package)</li>
-</ui>
+                    <div dangerouslySetInnerHTML={{__html: project.detailsDescription1}}></div>
                 </div>
                 <img className='detailsImage' src={process.env.PUBLIC_URL + project.detailsImage1} alt="image missing!" />    
             </div>
             <div className="secondSection">
                 <img className='detailsImage' src={process.env.PUBLIC_URL + project.detailsImage2} alt="image missing!" />
                 <div className="secondParagraph">
-                    <div>{project.detailsDescription1}</div>
+                    <div dangerouslySetInnerHTML={{__html: project.detailsDescription2}}></div>
                 </div>
-            </div>
+            </div> */}
         </div>
     ); /* TODO: Remove white line at bottom */
 }
