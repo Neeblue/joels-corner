3b3c63e4d565d32ddde47784d1464240af85dfa1
Joel Miles <98841710+Neeblue@users.noreply.github.com>
2023-09-25 16:50:40 +0000

    Removed ProjectCards and added code here

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,48 @@
-import ProjectCards from "../components/ProjectCards";
+import React, { useEffect, useState } from 'react';
+import jsonData from '../projects.json';
 
-export default function Projects() {
+const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  /*useEffect(() => {
+    // Fetch the GitHub projects data from your backend API
+    fetch('https://raw.githubusercontent.com/Neeblue/WebsiteData/main/data')
+      .then(response => response.json())
+      .then(data => setProjects(data))
+      .catch(error => console.error(error));
+  }, []);*/
+
+  useEffect(() => {
+    setProjects(jsonData);
+  }, []);
+
   return (
-    <div>
-      <ProjectCards />
+    <div className='projectCards'>      
+      <h1 className='header'>GitHub Projects</h1>
+      <div className='subHeader'>(Click on a project title to see more information about that project)</div>
+
+      {projects.map((project, index) => (
+        <div key={index} className="project-card">
+          
+          <div className='leftColumn'>
+            <img className='image' src={process.env.PUBLIC_URL + project.image} alt={project.title} />
+          </div>
+
+          <div className='rightColumn'>            
+            <h2 className='title'><a href={"/projects/" + project.keyword}>{project.title}</a></h2>
+            <div>
+              <a className='githubLink' href={project.link} target="_blank" rel="noopener noreferrer">
+                (Github)
+              </a>
+            </div>
+            <p className='description'>{project.description}</p>
+            {/* TODO: Add tags */}
+          </div>
+        </div>
+
+      ))}
     </div>
   );
-}
+};
+
+export default Projects;
