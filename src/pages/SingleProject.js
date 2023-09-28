663c8b5fce44c5a335af71ba8bfc4715fe077b1d
Joel Miles <98841710+Neeblue@users.noreply.github.com>
2023-09-28 17:52:19 +0000

    Improved single project display for mobile

--- src/pages/SingleProject.js
+++ src/pages/SingleProject.js
@@ -1,28 +1,41 @@
 import jsonData from '../projects.json';
 import './SingleProject.css';
 
+import { useMediaQuery } from 'react-responsive';
+
 export default function SingleProject(props){
     const project = jsonData.find(project => project.keyword === props.keyword);
+    const isMobile = useMediaQuery({ maxWidth: 768 });
 
     return (
         <div className="projectPage">
             <h1 className="title">{project.title}</h1>
 
             {project.detailsDescription.map((paragraph, index) => (
-                <div key={index}>
-                    {index % 2 === 0 ? (
-                        <div className='section'>
-                            <img className='detailsImage' src={process.env.PUBLIC_URL + project.detailsImage[index]} alt= "" />
-                            <div className="detailsParagraph" dangerouslySetInnerHTML={{__html: paragraph}}></div>
-                        </div>
-                        
-                    ) : (
-                        <div className='section'>
+                <div key={index} className='section'>
+                    {isMobile ? (
+                        <>
+                            <br></br>
                             <div className="detailsParagraph" dangerouslySetInnerHTML={{__html: paragraph}}></div>
                             <img className='detailsImage' src={process.env.PUBLIC_URL + project.detailsImage[index]} alt="" />
-                        </div>
+                            <br></br>
+                        </>
+                    ) : (
+                        <>
+                            {index % 2 === 0 ? (
+                                <>
+                                    <img className='detailsImage' src={process.env.PUBLIC_URL + project.detailsImage[index]} alt= "" />
+                                    <div className="detailsParagraph" dangerouslySetInnerHTML={{__html: paragraph}}></div>
+                                </>
+                            ) : (
+                                <>
+                                    <div className="detailsParagraph" dangerouslySetInnerHTML={{__html: paragraph}}></div>
+                                    <img className='detailsImage' src={process.env.PUBLIC_URL + project.detailsImage[index]} alt="" />
+                                </>
+                            )}
+                            <br></br>
+                        </>
                     )}
-                    
                 </div>
             ))}
             <br></br>
