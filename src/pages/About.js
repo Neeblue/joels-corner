3a90ddd1c31936e79a5e9796196a9553a4c5245d
Joel Miles <98841710+Neeblue@users.noreply.github.com>
2023-09-30 04:57:39 +0000

    About me page with data import and animations

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,53 @@
-import AboutMe from "../components/AboutMe";
-import EducationHistory from "../components/EducationHistory";
-import WorkHistory from "../components/WorkHistory";
-import Resume from "../components/Resume";
 import './About.css';
+import jsonData from '../about.json';
+import Collapsible from "react-collapsible";
+import { useEffect, useState } from 'react';
 
 export default function About() {
+    const [about, setAbout] = useState([]);
+    const [activeSection, setActiveSection] = useState(null);
+
+    useEffect(() => {
+        setAbout(jsonData);
+    }, []);
+
+    const toggleSection = (section) => {
+        if (activeSection === section) {
+            setActiveSection(null);
+        } else {
+            setActiveSection(section);
+        }
+    };
+
     return (
-        <div>
-            <div className="intro">This section contains information about myself. Click on a section to expand it.</div>
-            <div className="aboutMe">
-                <AboutMe />
-                <hr />
-                <EducationHistory />
-                <hr />
-                <WorkHistory />
-                <hr />
-                <Resume />
-            </div>
+        <div className='container'>
+            {Object.entries(about).map(([section, items]) => (
+                <div className='box' key={section}>
+                    <Collapsible
+                        key={section}
+                        trigger={
+                            <div
+                                className='section'
+                                onClick={() => toggleSection(section)}
+                                style={{ cursor: 'pointer' }}
+                            >
+                                {section}
+                            </div>
+                        }
+                        open={activeSection === section}
+                    >
+                        {items.map((item, index) => (
+                            <div key={index}>
+                                <h2 className='header'>{item.Header}</h2>
+                                <h3 className='subheader'>{item.Subheader}</h3>
+                                <div className='description'>{item.Description}</div>
+                            </div>
+                        ))}
+                    </Collapsible>
+                    <hr />
+                </div>
+            ))}
+            <br />
         </div>
-    )
+    );
 }
