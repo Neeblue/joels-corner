fb0bb6d26a7007e0585deca32606ac5d897ce5d7
Joel Miles <98841710+Neeblue@users.noreply.github.com>
2023-10-04 19:39:32 +0000

    Cleaned up code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,15 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <>
-            <nav className="nav">
-                {/* <a href="/" className="site-title">Joel's Corner</a> */}
-                <Link to="/" className="site-title">Joel's Corner</Link>
-                <ul>
-                    <li><Link to="/projects">Projects</Link></li>
-                    <li><Link to="/about">About me</Link></li>                
-                    <li><Link to="/contact">Contact me</Link></li>                
-                </ul>
-            </nav>
-        </>        
+        <nav className="nav">
+            {/* <a href="/" className="site-title">Joel's Corner</a> */}
+            <Link to="/" className="site-title">Joel's Corner</Link>
+            <ul>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/about">About me</Link></li>                
+                <li><Link to="/contact">Contact me</Link></li>                
+            </ul>
+        </nav>
+        
     )
 }
