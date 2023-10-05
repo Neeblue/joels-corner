73aa3d0c9df668afeba33c795df3ff11ea2d0483
Joel Miles <joelmiles@hotmail.co.uk>
2023-10-04 18:34:14 -0700

    Spacing after github link

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -19,25 +19,24 @@ const Projects = () => {
   }, []);
 
   return (
-    <div className='projects'>      
+    <div className='projects'>
       <h1 className='header'>GitHub Projects</h1>
       <div className='subHeader'>(Click on a project title to see more information about that project)</div>
 
       {projects.map((project, index) => (
         <div key={index} className="container">
-          
+
           <div className='imageContainer'>
             <img className='image' src={process.env.PUBLIC_URL + project.image} alt={project.title} />
           </div>
 
-          <div className='text'>            
+          <div className='text'>
             {/* <h2 className='title'><a href={"/projects/" + project.keyword}>{project.title}</a></h2> */}
             <h2 className='title'><Link to={`/projects/${project.keyword}`}>{project.title}</Link></h2>
-            <div>
-              <a className='githubLink' href={project.link} target="_blank" rel="noopener noreferrer">
-                (Github)
-              </a>
-            </div>
+
+            <a className='githubLink' href={project.link} target="_blank" rel="noopener noreferrer">
+              (Github)
+            </a>
             <p className='description'>{project.description}</p>
             {/* TODO: Add tags */}
           </div>
