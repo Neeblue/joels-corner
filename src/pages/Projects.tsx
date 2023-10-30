dd4f5b4d0b9226d2f7fb45d7ecbbb8a746dc666a
Joel Miles <98841710+Neeblue@users.noreply.github.com>
2023-10-30 18:55:36 +0000

    Changed to show projects in a grid

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -22,31 +22,31 @@ export default function Projects() {
     <div className='projects'>
       <h1 className='header'>GitHub Projects</h1>
       <div className='sub-header'>(Click on a project title to see more information about that project)</div>
+      <div className="project">
+        {projects.map((project, index) => (
+          <div key={index} className="container">
 
-      {projects.map((project, index) => (
-        <div key={index} className="container">
+            <div className='text'>
+              <h2 className='title'>
+                <Link to={`/projects/${project.keyword}`}>{project.title}</Link>
+              </h2>
 
-          <div className='imageContainer'>
-            
+              <a className='githubLink' href={project.link} target="_blank" rel="noopener noreferrer">
+                (Github)
+              </a>
+              <p className='description'>{project.description}</p>
+              {/* TODO: Add tags */}
+            </div>
+
+            <div className='imageContainer'>
               <Link to={`/projects/${project.keyword}`}>
                 <img className='image' src={process.env.PUBLIC_URL + project.image} alt={project.title} />
               </Link>
+            </div>
+            
           </div>
-
-          <div className='text'>
-            {/* <h2 className='title'><a href={"/projects/" + project.keyword}>{project.title}</a></h2> */}
-            <h2 className='title'>
-              <Link to={`/projects/${project.keyword}`}>{project.title}</Link>
-            </h2>
-
-            <a className='githubLink' href={project.link} target="_blank" rel="noopener noreferrer">
-              (Github)
-            </a>
-            <p className='description'>{project.description}</p>
-            {/* TODO: Add tags */}
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
