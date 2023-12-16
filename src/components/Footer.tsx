f08008801b469b3a4fa9ee398fec41cfecd8790e
Joel <joelmiles@hotmail.co.uk>
2023-12-16 08:08:32 -0800

    Added theme to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Footer.css';
+import { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 export default function Footer() {
     // A simple footer component that displays a clickable email address and a phone number in the bottom right corner of my site.
+    const theme = useContext(ThemeContext);
+
     return (
-        <div className='footer'>
+        <div className='footer' style={theme.theme}>
             <div>JoelMiles.com</div>
-            <a href='mailto: email@joelmiles.com'>
+            <a style={theme.theme} href='mailto: email@joelmiles.com'>
                 email@joelmiles.com
             </a>
         </div>
