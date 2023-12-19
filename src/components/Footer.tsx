570a193d333dee952ea532a537000ca1945e72ef
Joel <joelmiles@hotmail.co.uk>
2023-12-19 12:10:45 -0800

    Footer tailwind migration

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,12 +9,12 @@ export default function Footer() {
     const background = theme.theme.background === "#fff" ? "lightGray" : "rgba(0, 0, 0, 1)";
 
     return (
-        <div className='footer' style={{
+        <div className='fixed bottom-0 flex justify-between w-full p-2 bg-black' style={{
             ...theme.theme,
             background: background,
             }}>
             <div>JoelMiles.com</div>
-            <a style={theme.theme} href='mailto: email@joelmiles.com'>
+            <a style={theme.theme} className='text-nav-link' href='mailto: email@joelmiles.com'>
                 email@joelmiles.com
             </a>
         </div>
