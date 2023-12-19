5616641864d549bce11b4099f81aaa7d344b80d6
Joel <joelmiles@hotmail.co.uk>
2023-12-19 13:19:54 -0800

    Removed comment

--- src/components/ScrollToTopWrapper.tsx
+++ src/components/ScrollToTopWrapper.tsx
@@ -22,7 +22,6 @@ function ScrollToTop() {
         document.documentElement.scrollTo({
             top: 0,
             left: 0,
-            // behavior: "instant",
         });
     }, [pathname]);
 
