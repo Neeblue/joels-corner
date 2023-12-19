316fe576f01bfa4f3215c7d377bf896cebba2119
Joel <joelmiles@hotmail.co.uk>
2023-12-19 12:08:31 -0800

    Intro tailwind migration

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,16 +1,14 @@
 import React, { Link } from 'react-router-dom';
-import './Introduction.css';
 
-export default function Introduction() {    
+export default function Introduction() {
     return (
-        <div className="intro">
-            <div className="intro-text">Welcome to my webpage! Here you can find a list of projects that I have worked on,
-            my resume, a way to contact me, and more information about myself!</div>
+        <div className="text-center mb-8">
+            <div className="text-xl mb-4">Welcome to my webpage! Here you can find a list of projects that I have worked on, and more information about myself!</div>
             {/* TODO: Add icon tags, Github main link */}
-            <div className="buttons">
-                <Button url = "/projects" name="Projects" img="/images/projects-icon.png" />
-                <Button url = "/about" name="About me" img="/images/about-icon.png"/>
-                {/* <Button url="/contact" name={`Contact me (unused)`} img="/images/contact-icon.png" /> */}
+            <div className="flex flex-col sm:flex-row sm:justify-center">
+                <Button url="/projects" name="Projects" img="/images/projects-icon.png" />
+                <Button url="/about" name="About me" img="/images/about-icon.png" />
+                <Button url="/contact" name="Contact me" img="/images/contact-icon.png" />
             </div>
             <br />
         </div>
@@ -19,10 +17,10 @@ export default function Introduction() {
 
 function Button (props: {url: string, name: string, img: string}) {
     return (
-        <div className='button'>
-            <Link to={props.url}>
-                <img src={process.env.PUBLIC_URL + props.img} alt={props.name} />
-                <h1>{props.name}</h1>
+        <div className='max-sm:mx-auto'>
+            <Link className='hover:bg-transparent' to={props.url}>
+                <img className='w-[50vw] sm:w-[20rem]' src={process.env.PUBLIC_URL + props.img} alt={props.name} />
+                <div className='decoration-0 text-2xl'>{props.name}</div>
             </Link>
         </div>
     );
