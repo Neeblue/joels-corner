82333748d51cf90f044ee670046a294d5a6514c7
Joel <joelmiles@hotmail.co.uk>
2023-12-19 12:35:54 -0800

    Navbar migration to use class based theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,15 @@ import ToggleTheme from './ToggleTheme';
 
 export default function Navbar() {
     const theme = useContext(ThemeContext);
-    const background = theme.theme.background === "#fff" ? "lightGray" : "rgba(0, 0, 0, 1)";
+    const background = theme.theme.includes("#fff") ? "lightGray" : "rgba(0, 0, 0, 1)";
     const values = "font-bold text-lg color-nav-link max-sm:mx-auto";
     const focus = "focus:bg-link-focus focus:text-black text-nav-link";
-    
+
     return (
-        <nav className="bg-black w-full flex justify-between py-2 px-4 mb-4 sm:flex-column max-sm:flex-col" style={{
-            ...theme.theme,
-            padding: "0.5rem", 
-            background: background,
-        }}>
-            <div style={{ display: "flex", flexDirection: "row", gap: "0.5rem" }}>
+        <nav className={`bg-black w-full flex justify-between py-2 px-4 mb-4 sm:flex-column max-sm:flex-col ${theme.theme}`}
+            // Theme override for app background
+            style={{ padding: "0.5rem", background: background, }}>
+            <div className='flex flex-row gap-2'>
                 <ToggleTheme />
                 <Link to="/" className={`${values} ${focus} text-[2rem] max-sm:mb-2`}>Joel's Corner</Link>
             </div>
