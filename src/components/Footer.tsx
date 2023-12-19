a168b3a51e933bcc51595149b7fbc4e07367b695
Joel <joelmiles@hotmail.co.uk>
2023-12-19 12:42:38 -0800

    Footer migration to use class theme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,15 +6,12 @@ import { ThemeContext } from '../contexts/ThemeContext';
 export default function Footer() {
     // A simple footer component that displays a clickable email address and a phone number in the bottom right corner of my site.
     const theme = useContext(ThemeContext);
-    const background = theme.theme.background === "#fff" ? "lightGray" : "rgba(0, 0, 0, 1)";
+    const background = theme.theme.includes("#fff") ? "lightGray" : "rgba(0, 0, 0, 1)";
 
     return (
-        <div className='fixed bottom-0 flex justify-between w-full p-2 bg-black' style={{
-            ...theme.theme,
-            background: background,
-            }}>
+        <div className={`fixed bottom-0 flex justify-between w-full p-2 bg-black ${theme.theme}`} style={{ background: background, }}>
             <div>JoelMiles.com</div>
-            <a style={theme.theme} className='text-nav-link' href='mailto: email@joelmiles.com'>
+            <a className={`text-nav-link ${theme.theme}`} href='mailto: email@joelmiles.com'>
                 email@joelmiles.com
             </a>
         </div>
