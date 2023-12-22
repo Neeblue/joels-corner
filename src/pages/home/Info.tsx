ef749ed75ae499d6404f7155b51e8875ca68e82b
Joel Miles <98841710+Neeblue@users.noreply.github.com>
2023-12-22 20:05:34 +0000

    Modified info about myself

--- src/pages/home/Info.tsx
+++ src/pages/home/Info.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 export default function Info() {
     return (
-        <div className="text-center">
-            <div className="text-xl">Welcome to my webpage! Here you can find a list of projects that I have worked on, and more information about myself!</div>
+        <div className='ml-4 max-w-[90vw]'>
+            <div className='contents'>
+                <div className='text-4xl mb-2'>Hello!</div>
+                <hr />
+            </div>
+            <div className="text-xl max-w-[90vw]">My name is Joel Miles, and I'm a long term on-again, off-again C# hobby coder.</div>
             <br />
         </div>
     );
