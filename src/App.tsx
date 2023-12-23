715d17358c76248cfd80d026a5523baf646696d7
Joel Miles <joelmiles@hotmail.co.uk>
2023-12-22 16:19:53 -0800

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { ThemeProvider } from './contexts/ThemeContext';
 import Homepage from "./pages/Homepage";
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 function App() {
 
   return (
     <ThemeProvider>
-      <BrowserRouter>
-        <div className="App min-h-[100vh]">
-          <Homepage />
-        </div >
-      </BrowserRouter>
+      <MantineProvider>
+        <BrowserRouter>
+          <div className="App min-h-[100vh]">
+            <Homepage />
+          </div >
+        </BrowserRouter>
+      </MantineProvider>
     </ThemeProvider>
   )
 }
