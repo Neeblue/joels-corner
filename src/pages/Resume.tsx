dc41cb8aead9b6691c483e912f8f077653394135
Joel Miles <joelmiles@hotmail.co.uk>
2024-01-02 11:19:08 -0800

    Resume larger on larger screens

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -3,16 +3,20 @@ import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function Resume() {
+    const scale = window.innerWidth < 768 ? 1 : 1.5;
     return (
-        <div className="max-w-full mx-auto max-h-full overflow-auto w-[612px]">
-            <Document file={'/Resume.pdf'}>
-                <Page
-                    pageNumber={1}
-                    renderTextLayer={false}
-                    renderAnnotationLayer={false}
-                    renderForms={false}       
-                />
-            </Document>
-        </ div>
+        <>
+            <div className=" max-w-full mx-auto md:m-4 md:mx-auto max-h-full overflow-auto w-[612px] md:w-[918px]">
+                <Document file={'/Resume.pdf'}>
+                    <Page
+                        pageNumber={1}
+                        renderTextLayer={false}
+                        renderAnnotationLayer={false}
+                        renderForms={false}
+                        scale={scale}
+                    />
+                </Document>
+            </ div>
+        </>
     )
 }
