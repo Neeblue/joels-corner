89aa34ead1ca8fa1022bbcd17f94e08b095f1d7f
Joel Miles <joelmiles@hotmail.co.uk>
2024-01-24 21:15:53 -0800

    Corrected text

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -22,7 +22,7 @@ export default function Projects() {
   return (
     <div className='projects-page text-center'>
       <h1 className='header text-3xl font-bold text-primary'>GitHub Projects</h1>
-      <div className='sub-header text-primary mb-8 mx-2'>(Click on a project title to see more information about that project)</div>
+      <div className='sub-header text-primary mb-8 mx-2'>(Click on a project title or image to see more information about that project)</div>
       <div className="projects flex flex-col flex-wrap justify-center items-center sm:items-start gap-8 mb-12 sm:flex-row sm:flex">
         {projects.map((project, index) => (
           <div key={index}>
