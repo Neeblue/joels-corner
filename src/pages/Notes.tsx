ffb68c780bcfd35d3d59270dd3cc9d7057f1293a
Joel Miles <joelmiles@hotmail.co.uk>
2024-02-11 14:04:54 -0800

    Added button to notes page

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import NotesCard from '../components/NotesCard';
 
 interface Note {
@@ -11,21 +11,26 @@ export default function Notes() {
     const [notes, setNotes] = useState<Note[]>([]);
     const [selectedLabel, setSelectedLabel] = useState<string>('');
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isButtonActive, setIsButtonActive] = useState<boolean>(true);
 
-    /// Real data
-    useEffect(() => {
+    const handleButtonPress = useCallback(() => {
         setIsLoading(true);
+        console.log('Fetching notes...');
+        setIsButtonActive(false);
+        
         fetch(`https://post-message-neeblue.azurewebsites.net/api/notes?code=${import.meta.env.VITE_APP_AZURE_FUNCTION_API}`)
-            .then(response => response.json())
-            .then(data => {
-                setNotes(data);
-                setIsLoading(false);
-            })
-            .catch(error => {
-                console.error(error);
-                setIsLoading(false);
-            });
-    }, []);
+        .then(response => response.json())
+        .then(data => {
+            setNotes(data);
+            setIsLoading(false);
+            console.log('Notes fetched.');
+        })
+        .catch(error => {
+            console.error(error);
+            setIsLoading(false);
+        });
+
+    }, [isButtonActive]);
 
     const handleLabelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSelectedLabel(event.target.value);
@@ -37,6 +42,13 @@ export default function Notes() {
         <div>
             <h1 className='text-center text-primary'>Notes to myself</h1>
             <div className='text-center text-primary mb-8'>These notes are pulled from an Azure database using an Azure Function endpoint. The free tier of Azure Functions has a long cold start, so results may take a while to display.</div>
+
+            {isButtonActive && (
+                <div className='text-center mb-4'>
+                    <button className='btn btn-primary' onClick={handleButtonPress}>Fetch notes</button>
+                </div>
+            )}
+
             <div className='flex flex-wrap gap-4 justify-center mb-4'>
                 <input type="radio" aria-label="All" className="btn" value='' checked={selectedLabel === ''} onChange={handleLabelChange} />
                 <input type="radio" aria-label="Todos" className="btn" value='Todo' checked={selectedLabel === 'Todo'} onChange={handleLabelChange} />
