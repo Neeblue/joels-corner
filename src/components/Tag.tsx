942c0e2f0ed93db00f9448d87e58e8cee40c40dc
Joel Miles <joelmiles@hotmail.co.uk>
2024-10-25 10:53:27 -0700

    style: bolder tags for projects

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,6 +1,6 @@
 export default function Tag(props: { name: string }) {
     return (
-        <div className="tag bg-secondary text-neutral py-[0.2rem] px-[0.4rem] rounded-full inline-block m-[0.3rem] text-[0.7rem]">
+        <div className="tag bg-secondary text-neutral py-[0.2rem] px-[0.4rem] rounded-full inline-block m-[0.3rem] text-[0.7rem] font-semibold">
             {props.name}
         </div>
     )
